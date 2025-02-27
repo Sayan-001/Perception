545cb482951bea04de9625894db7b10a6ea77926
Sayan Gupta <sayangupta840@gmail.com>
2025-02-27 11:32:14 +0530

    modified vars

--- peak-app/src/firebase.js
+++ peak-app/src/firebase.js
@@ -1,18 +1,16 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyD6m8p8evFYxIU60ZQpjjaVOO4rVe8hyVI",
-  authDomain: "doubleslash-1.firebaseapp.com",
-  projectId: "doubleslash-1",
-  storageBucket: "doubleslash-1.firebasestorage.app",
-  messagingSenderId: "478224830594",
-  appId: "1:478224830594:web:4fda3ea234ed4f08bf249b",
-  measurementId: "G-8LFVR9FB7R",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-const db = getFirestore(app);
-export { auth, db };
+export { auth };
